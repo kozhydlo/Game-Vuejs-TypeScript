a508ba4c8a9cf693ce906d16e0b319b9dc04f588
Mark Kozhydlo <99179725+kozhydlo@users.noreply.github.com>
2024-08-28 19:55:29 +0300

    Update GamePopup.vue

--- src/components/GamePopup.vue
+++ src/components/GamePopup.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import type { GameStatus } from '../types/GameStatus'
+import type { GameStatus } from '../Types/GameStatus'
 
 interface Props {
   word: string
